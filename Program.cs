fcd8cffc2c89f0ade17ace2c5922b8500e928ad2
HungTran <trannguyenhung@ids.company>
2023-04-13 11:42:39 -0500

    update:
    Now using parralel.foreach to improve runtime and improve Readme.MD

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Collections.Concurrent;
 
 namespace Gritango
 {
     class Program
     {
-        public static void Main(string[] args){   
+        public static void Main(string[] args)
+        {
             var watch = new System.Diagnostics.Stopwatch();
             ///replace this with one/multiple resume and add a loop to each find skill function
             string resume = "Software development professional with experience in developing and designing web applications using HTML, CSS, JavaScript, Node.js, React Native, Angular, Dart, Flutter and React. Adept at developing and deploying complex backend systems, web services and databases, using both MEAN and MERN stack\nSoftware development professional with experience in developing and designing web applications using HTML, CSS, JavaScript,Node.js, React Native, Angular,, Dart, Flutter and React. Adept at developing and deploying complex backend systems, web services and databases, using both MEAN and MERN stack.Software development professional with experience in developing and designing web applications using HTML, CSS, JavaScript,Node.js, React Native, Angular,, Dart, Flutter and React. Adept at developing and deploying complex backend systems, web services and databases, using both MEAN and MERN stack.Software development professional with experience in developing and designing web applications using HTML, CSS, JavaScript,Node.js, React Native, Angular,, Dart, Flutter and React. Adept at developing and deploying complex backend systems, web services and databases, using both MEAN and MERN stack.Software development professional with experience in developing and designing web applications using HTML, CSS, JavaScript,Node.js, React Native, Angular,, Dart, Flutter and React. Adept at developing and deploying complex backend systems, web services and databases, using both MEAN and MERN stack.Software development professional with experience in developing and designing web applications using HTML, CSS, JavaScript,Node.js, React Native, Angular,, Dart, Flutter and React. Adept at developing and deploying complex backend systems, web services and databases, using both MEAN and MERN stack.Software development professional with experience in developing and designing web applications using HTML, CSS, JavaScript,Node.js, React Native, Angular,, Dart, Flutter and React. Adept at developing and deploying complex backend systems, web services and databases, using both MEAN and MERN stack.Software development professional with experience in developing and designing web applications using HTML, CSS, JavaScript,Node.js, React Native, Angular,, Dart, Flutter and React. Adept at developing and deploying complex backend systems, web services and databases, using both MEAN and MERN stack.";
@@ -19,10 +19,6 @@ namespace Gritango
 
             string[] normal = FindSkillsinResume(resume);
 
-            // Console.WriteLine("\nSkills found: ");
-            // foreach (string skill in normal){
-            //     Console.WriteLine(skill);
-            // }
             watch.Stop();
 
             Console.WriteLine("Without performance improvements");
@@ -30,42 +26,42 @@ namespace Gritango
 
             watch.Restart();
             string[] improved = FindSkillsinResumeImproved(resume);
-            // Console.WriteLine("\nSkills found (improved version): ");
-            // foreach(string skill in improved){
-            //     Console.WriteLine(skill);
-            // }
+
             watch.Stop();
             Console.WriteLine("With performance improvements");
             Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
 
         }
 
-        public static string[] FindSkillsinResume(string resume){
+        public static string[] FindSkillsinResume(string resume)
+        {
             //just a list of tring that you want to filtered from a resume
-            string[] skills = {"HTML", "CSS","JavaScript", "Node.js", "Angular", "Dart", "Python", "Java","C#", "ASP.NET", "Flutter" };
+            string[] skills = { "HTML", "CSS", "JavaScript", "Node.js", "Angular", "Dart", "Python", "Java", "C#", "ASP.NET", "Flutter" };
             var foundSkills = new HashSet<string>();
-            foreach (string keyword in skills){
-                if(resume.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0){
+            foreach (string keyword in skills)
+            {
+                if (resume.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
                     foundSkills.Add(keyword);
+
                 }
             }
             return foundSkills.ToArray();
         }
         //function improvement using simple regex search
-        public static string [] FindSkillsinResumeImproved(string resume){
-            //same list
-            string[] skills = {"HTML", "CSS","JavaScript", "Node.js", "Angular", "Dart", "Python", "Java","C#", "ASP.NET", "Flutter" };
-            string pattern = string.Join("|", skills.Select(Regex.Escape));
-            
-            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+        public static string[] FindSkillsinResumeImproved(string resume)
+        {
 
-            var foundSkills = new HashSet<string>();
+            string[] skills = { "HTML", "CSS", "JavaScript", "Node.js", "Angular", "Dart", "Python", "Java", "C#", "ASP.NET", "Flutter" };
+            var foundSkills = new ConcurrentBag<string>();
 
-            foreach (Match match in regex.Matches(resume)){
-                string skill = match.Value;
-                bool added = foundSkills.Add(skill);
-            }
-
+            Parallel.ForEach(skills, keyword =>
+            {
+                if (resume.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    foundSkills.Add(keyword);
+                }
+            });
             return foundSkills.ToArray();
         }
     }
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Dotnet-Project-Runtime-Measurement
 
-This program has to version, a normal and improve one. The purpose of this program is to measure the runtime of each method. 
+This program has to version, a normal and improve one. The purpose of this program is to measure the runtime of each method.
 The resume input in this project is currently in a form of a string, you can modify it a bit to make it works a large number of resume.
+
+There is a sln file in this project.
+To run the code, simply clone this repo to your local machine,
+run `dotnet build` and `dotnet run`.
